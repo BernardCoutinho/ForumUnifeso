5be7de63bbdd54ad99f701904b4e9ba573a051ed
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-08-30 09:53:47 -0300

    Criando entidades base.

---
+++ ForumUnifeso/src/API/Model/Person/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ForumUnifeso.src.API.Model.Person
+{
+    public class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ ForumUnifeso/src/API/Model/Post/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace ForumUnifeso.src.API.Model.Post
+{
+    using ForumUnifeso.src.API.Model.Person;
+    public class Post
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Date{ get; private set; }
+        public Person Author{ get; private set; }
+    }
+}
