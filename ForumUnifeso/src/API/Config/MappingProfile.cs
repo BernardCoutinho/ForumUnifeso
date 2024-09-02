c5c188590da2493a9fc6086abed0e159e56f1613
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-09-02 16:40:49 -0300

    FU-00002 - Arquivo de configuração de mapeamento

---
+++ ForumUnifeso/src/API/Config/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ForumUnifeso.src.API.Model;
+using ForumUnifeso.src.API.View;
+namespace ForumUnifeso.src.API.Config
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Person, PersonResponse>();
+            CreateMap<PersonRequest, Person>();
+
+            CreateMap<PostRequest, Post>();
+            CreateMap<Post, PostResponse>();
+        }
+    }
+}
