ee5d9fe5976bbc5f02602c95d40146d397356a24
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-09-02 16:41:50 -0300

    FU-00002 - DTOs de Person e Post

---
+++ ForumUnifeso/src/API/View/Post/PostResponse.cs
@@ -0,0 +1,15 @@
+﻿using ForumUnifeso.src.API.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace ForumUnifeso.src.API.View
+{
+    public class PostResponse
+    {
+        public string Title { get; private set; }
+       
+        public string Description { get; private set; }
+        public DateTime Date { get; private set; }
+
+        public PersonResponse Author { get; private set; }
+    }
+}
