597c886b22303247e81f1eb4c58ba550f388a964
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-09-05 17:41:43 -0300

    Endpoints GetAll, GetById, Post, Put, Delete e DeleteById de Post

--- ForumUnifeso/src/API/Controller/Post/PostController.cs
+++ ForumUnifeso/src/API/Controller/Post/PostController.cs
@@ -31,10 +31,140 @@ namespace ForumUnifeso.src.API.Controller
             {
                 Post post = _mapper.Map<Post>(postRequest);
 
-               await _postService.AddAsync(post);
+                await _postService.AddAsync(post);
 
                 PostResponse response = _mapper.Map<PostResponse>(post);
-                return CreatedAtAction(nameof(PostResponse), response);
+                return CreatedAtAction(nameof(GetPostById), new { id = post.Id }, response);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(500, "Mapping configuration issue.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPostById(int id)
+        {
+            try
+            {
+                var post = await _postService.GetByIdAsync(id);
+                if (post == null)
+                {
+                    return NotFound();
+                }
+
+                PostResponse response = _mapper.Map<PostResponse>(post);
+
+                return Ok(response);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(500, "Mapping configuration issue.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllPost()
+        {
+
+            try
+            {
+                IEnumerable<Post> post = await _postService.GetAllAsync();
+
+                IEnumerable<PostResponse> response = _mapper.Map<IEnumerable<PostResponse>>(post);
+
+                return Ok(response);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(500, "Mapping configuration issue.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdatePostAsync([FromBody] PostRequest postRequest)
+        {
+            if (postRequest is null)
+            {
+                return BadRequest("Post data is null.");
+            }
+
+            try
+            {
+                Post post = _mapper.Map<Post>(postRequest);
+
+                await _postService.UpdateAsync(post);
+
+                PostResponse response = _mapper.Map<PostResponse>(post);
+                return Ok(response);
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(500, "Mapping configuration issue.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePostAsync([FromBody] PostRequest postRequest)
+        {
+            if (postRequest is null)
+            {
+                return BadRequest("Post data is null.");
+            }
+
+            try
+            {
+                Post post = _mapper.Map<Post>(postRequest);
+
+                await _postService.DeleteAsync(post);
+
+                PostResponse response = _mapper.Map<PostResponse>(post);
+                return NoContent();
+            }
+            catch (AutoMapperMappingException ex)
+            {
+                return StatusCode(500, "Mapping configuration issue.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePostByIdAsync(int id)
+        {
+            if (await _postService.GetByIdAsync(id) is not null)
+            {
+                return BadRequest("Post data is null.");
+            }
+
+            try
+            {
+
+                await _postService.DeleteByIdAsync(id);
+
+                return NoContent();
             }
             catch (AutoMapperMappingException ex)
             {
