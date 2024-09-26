26d2b8532a3db9089d39b9cb35de8030de754d83
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-09-26 18:48:31 -0300

    :sparkles: UF-00007 - Configuraçãod e Jwt na Program.cs

--- ForumUnifeso/Program.cs
+++ ForumUnifeso/Program.cs
@@ -3,11 +3,14 @@ using ForumUnifeso.src.API.Base.Context;
 using ForumUnifeso.src.API.Interface;
 using ForumUnifeso.src.API.Repository;
 using ForumUnifeso.src.API.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddScoped<IThreadForumService, ThreadForumService>();
@@ -26,9 +29,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PrincipalDbContext>(opt =>
-    // opt.UseInMemoryDatabase("InMemoryDb")
-    opt.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True")
+     opt.UseInMemoryDatabase("InMemoryDb")
+    //opt.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=master;Trusted_Connection=True;TrustServerCertificate=True")
     );
+
+builder.Services.AddAuthentication((opt) =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer((opt) =>
+{
+    opt.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+
+    };
+});
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -41,6 +66,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
