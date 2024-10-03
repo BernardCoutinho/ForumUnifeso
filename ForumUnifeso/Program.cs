a52548a4c40b83ffb23d2484b11f2976806d2bcf
marcelaOliveiraOMP <chiizurag@gmail.com>
2024-10-02 23:35:31 -0300

    teste

--- ForumUnifeso/Program.cs
+++ ForumUnifeso/Program.cs
@@ -36,11 +36,14 @@ try
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+    // if (app.Environment.IsDevelopment())
+    // {
+    //     app.UseSwagger();
+    //     app.UseSwaggerUI();
+    // }
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
     app.UseHttpsRedirection();
 
